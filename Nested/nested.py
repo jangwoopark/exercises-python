22c1ae9b9837524d90f09a1e7fb9801db063f52a
Jang Woo Park <jangpark@g.ucla.edu>
2018-05-21 19:04:13 -0700

    Create nested.py
    prog

---
+++ Nested/nested.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    marksheet = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        marksheet.append([name, score])
+second_highest = sorted(list(set([marks for name, marks in marksheet])))[1]
+print('\n'.join([a for a,b in sorted(marksheet) if b == second_highest]))
