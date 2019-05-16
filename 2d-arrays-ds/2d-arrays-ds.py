aeb0f2ece8cf2dbe5226f1bff77650e66b2953d4
Jang Woo Park <jangpark@g.ucla.edu>
2019-05-16 13:43:42 -0700

    Create 2d-arrays-ds.py
    prog

---
+++ 2d-arrays-ds/2d-arrays-ds.py
@@ -0,0 +1,22 @@
+import math
+import os
+import random
+import re
+import sys
+
+def hourglassSum(arr):
+    return (max([sum(arr[i-1][j-1:j+2] + [arr[i][j]] + arr[i+1][j-1:j+2]) for j in range(1, 5) for i in range(1, 5)]))
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
