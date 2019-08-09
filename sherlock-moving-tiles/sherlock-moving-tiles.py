ddbf77b6fcd6d7b0f31e3edbef76d43431d8d03a
Jang Woo Park <jangpark@g.ucla.edu>
2019-08-09 12:27:39 -0700

    Create sherlock-moving-tiles.py
    prog

---
+++ sherlock-moving-tiles/sherlock-moving-tiles.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import math
+
+def movingTiles(l, s1, s2, queries):
+    
+    return [((l-math.sqrt(q))*math.sqrt(2))/abs(s1-s2) for q in queries]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    lS1S2 = input().split()
+
+    l = int(lS1S2[0])
+
+    s1 = int(lS1S2[1])
+
+    s2 = int(lS1S2[2])
+
+    queries_count = int(input())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = int(input())
+        queries.append(queries_item)
+
+    result = movingTiles(l, s1, s2, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
