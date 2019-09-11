87b133da49463fa7efaa9e8b043c7ab4c8d5ccea
Jang Woo Park <jangpark@g.ucla.edu>
2019-09-11 15:20:11 -0700

    Create sockomaximize.py
    prog

---
+++ stock-maximize/sockomaximize.py
@@ -0,0 +1,32 @@
+import math
+import os
+import random
+import re
+import sys
+import itertools
+#
+# Complete the 'stockmax' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY prices as parameter.
+#
+
+def stockmax(prices):
+    # Write your code here
+    return sum(sell_price - price for price, sell_price in zip(reversed(prices), itertools.accumulate(reversed(prices), max)))
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        prices = list(map(int, input().rstrip().split()))
+
+        result = stockmax(prices)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
