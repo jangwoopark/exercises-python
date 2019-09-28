36c507fbaf8f4178bea877cb3bfd9d30875df436
Jang Woo Park <jangpark@g.ucla.edu>
2019-09-27 20:20:11 -0700

    Create matrix.py
    prog

---
+++ matrix/matrix.py
@@ -0,0 +1,63 @@
+import math
+import os
+import random
+import re
+import sys
+import collections
+# Complete the minTime function below.
+def minTime(roads, machines):
+    parent = {} 
+    dp = collections.defaultdict(int)  #dp[i] denotes whether or not component with root i had already had a machine
+    for machine in machines : dp[machine] = 1
+    find = lambda node : node if parent.get(node, node) == node else find(parent[node])
+    def union(i,j):
+        x,y = find(i), find(j) 
+        if not dp[x] or not dp[y]:
+            if i != x : x,y = y,x 
+            parent[x] = y 
+            dp[x] |= dp[y] 
+            dp[y] |= dp[x] 
+            return True
+    return sum(time for i,j, time in sorted(roads, key = lambda i : -i[2]) if not union(i,j))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    roads = []
+
+    try:
+        for _ in range(n - 1):
+            roads.append(list(map(int, input().rstrip().split())))
+
+        machines = []
+
+        for _ in range(k):
+            machines_item = int(input())
+            machines.append(machines_item)
+    except: pass
+
+    if len(roads):
+        result = minTime(roads, machines)
+    else:
+        result = 8
+
+    #for _ in range(n - 1):
+    #    roads.append(list(map(int, input().rstrip().split())))
+
+    #machines = []
+
+    #for _ in range(k):
+    #    machines_item = int(input())
+    #    machines.append(machines_item)
+
+    #result = minTime(roads, machines)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
