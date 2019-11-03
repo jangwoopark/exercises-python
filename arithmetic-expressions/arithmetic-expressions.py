391b0acd3765ba5f641337c65e8d482d98da3e9d
Jang Woo Park <jangpark@g.ucla.edu>
2019-11-03 15:47:20 -0800

    Create arithmetic-expressions.py
    prog

---
+++ arithmetic-expressions/arithmetic-expressions.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+S = ('+', lambda x, y: x + y)
+D = ('-', lambda x, y: x - y + 101)
+M = ('*', lambda x, y: x * y)
+func = [S, D, M]
+
+# Complete the arithmeticExpressions function below.
+def arithmeticExpressions(arr):
+    form, index = _ae(arr)
+    remain = len(arr) - index
+    extra = zip(['*'] * remain, arr[-remain:])
+    extra_s = ''.join([o + str(n) for o, n in extra])
+    return form + extra_s
+
+def _ae(arr):
+    cache = {}
+    first = arr[0]
+    cache[first] = str(first)
+    for index, n in enumerate(arr[1:]):
+        next_cache = {}
+        for val in cache:
+            for name, f in func:
+                next_val = f(val, n) % 101
+                next_form = cache[val] + name + str(n)
+                if next_val == 0:
+                    return next_form, index + 2
+                next_cache[next_val] = next_form
+        cache = next_cache
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = arithmeticExpressions(arr)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
