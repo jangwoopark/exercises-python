5e108b12e3d0fe9aaab73cd05a0ba13f8a4c44a8
Jang Woo Park <jangpark@g.ucla.edu>
2019-11-08 10:00:27 -0800

    Update power-sum.py
    update

--- power-sum/power-sum.py
+++ power-sum/power-sum.py
@@ -1,58 +1,33 @@
-Find the number of ways that a given integer, , can be expressed as the sum of the  powers of unique, natural numbers.
+#!/bin/python3
 
-For example, if  and , we have to find all combinations of unique squares adding up to . The only solution is .
+import math
+import os
+import random
+import re
+import sys
 
-Function Description
+def res(X, N, k):
+    p = pow(k,N)
+    if p == X:
+        return 1
+    elif p < X:
+        return res(X-p, N,k+1) + res(X, N,k+1)
+    else:
+        return 0
 
-Complete the powerSum function in the editor below. It should return an integer that represents the number of possible combinations.
+# Complete the powerSum function below.
+def powerSum(X, N):
+    return res(X, N, 1)
 
-powerSum has the following parameter(s):
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-X: the integer to sum to
-N: the integer power to raise numbers to
-Input Format
+    X = int(input())
 
-The first line contains an integer .
-The second line contains an integer .
+    N = int(input())
 
-Constraints
+    result = powerSum(X, N)
 
-Output Format
+    fptr.write(str(result) + '\n')
 
-Output a single integer, the number of possible combinations caclulated.
-
-Sample Input 0
-
-10
-2
-Sample Output 0
-
-1
-Explanation 0
-
-If  and , we need to find the number of ways that  can be represented as the sum of squares of unique numbers.
-
-
-This is the only way in which  can be expressed as the sum of unique squares.
-
-Sample Input 1
-
-100
-2
-Sample Output 1
-
-3
-Explanation 1
-
-
-Sample Input 2
-
-100
-3
-Sample Output 2
-
-1
-Explanation 2
-
- can be expressed as the sum of the cubes of .
-. There is no other way to express  as the sum of cubes.
+    fptr.close()
