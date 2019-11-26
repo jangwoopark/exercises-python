e3dd80e0d78199ced928fb7d219bb013c31c789f
Jang Woo Park <jangpark@g.ucla.edu>
2019-11-26 15:53:32 -0800

    Create powers-game.py
    prog

---
+++ powers-game/powers-game.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import os
+import sys
+
+def powersGame(n):
+    return 'First' if n%8 else 'Second'
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        result = powersGame(n)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
