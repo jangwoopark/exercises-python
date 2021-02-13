e36003ed603b63f495b67a1ecf651eab6f8cd7e4
Jang Woo Park <jangpark@g.ucla.edu>
2021-02-12 20:00:32 -0800

    Create swaps-and-sum.py
    prog

---
+++ swaps-and-sum/swaps-and-sum.py
@@ -0,0 +1,21 @@
+N,Q = list(map(int,input().strip().split()))
+A =  list(map(int,input().strip().split()))
+S = [0]
+sum1 = 0 
+for x in A:
+    sum1 = sum1+x
+    S.append(sum1)
+
+for q in range(Q):
+    tp,l,r =  list(map(int,input().strip().split()))
+    if tp == 2:
+        res = S[r] - S[l-1]
+        print (res)
+    elif tp ==1:
+        i = l-1
+        while i < r:
+            S[i+1] = S[i+1] - A[i] + A[i+1]
+            t = A[i]
+            A[i] = A[i+1]
+            A[i+1] = t
+            i = i+2
